59828d005ce47a7258709d0cbd2a81d2332daeca
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-08-14 00:55:10 +0300

    update records

---
+++ app/controllers/records_controller.rb
@@ -0,0 +1,28 @@
+class RecordsController < ApplicationController
+  def index
+    @family = Family.find(1)
+    @user = @family.user.find(1)
+    @records = @family.record.all.order(:date)
+  end
+
+  def create
+    @record = Record.new(record_params)
+
+    if @record.save
+      redirect_to records_path
+    else
+      render 'index'
+    end
+  end
+
+  def destroy
+    @record = Record.find(params[:id])
+    @record.destroy
+
+    redirect_to records_path
+  end
+
+  def record_params
+    params.require(:record).permit(:date, :description, :item_id, :value, :user_id)
+  end
+end
