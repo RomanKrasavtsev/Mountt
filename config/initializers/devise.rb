96310a0c5aafa7cbfcc778278076903978f45952
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-08-20 16:16:41 +0300

    rename to Mountt

--- config/initializers/devise.rb
+++ config/initializers/devise.rb
@@ -12,7 +12,7 @@ Devise.setup do |config|
   # Configure the e-mail address which will be shown in Devise::Mailer,
   # note that it will be overwritten if you use your own mailer class
   # with default "from" parameter.
-  config.mailer_sender = 'notreply@familybudget.com'
+  config.mailer_sender = 'hello@mountt.ru'
 
   # Configure the class responsible to send e-mails.
   config.mailer = 'Devise::Mailer'
