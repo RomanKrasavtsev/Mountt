9d29a673e5be85d4d834c3f66e0553a52b07c4e7
Roman Krasavtsev <mr.krasavtsev@gmail.com>
2016-09-14 01:20:34 +0300

    update dashboard

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,14 +2,22 @@ class DashboardController < ApplicationController
   before_action :authenticate_user!
 
   def index
+    # Records
     records = current_user.family.record
       .current_month
       .select("records.name", "items.value")
       .joins(:item)
       .group("items.name")
-      .order("items_name")
       .sum("records.value")
+    @records = records.map { |k, v| { name: k , value: v } }
 
-    @dataset = records.map { |k, v| { name: k , value: v } }
+    # Users
+    users = current_user.family.record
+      .current_month
+      .select("users.firstname", "items.value")
+      .joins(:item)
+      .group("users.firstname")
+      .sum("records.value")
+    @users = users.map { |k, v| { user: k, amount: v } }
   end
 end
